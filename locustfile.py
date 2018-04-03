46158ee7e3126d6b26099209640f7d62e5a44b69
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-04-03 13:43:09 -0300

    Fixed locust file

--- locustfile.py
+++ locustfile.py
@@ -1,52 +1,16 @@
-# import os
-# from locust import HttpLocust
-# from tasks import StudentTaskSet, TeacherTaskSet
-
-# class BaseHttpLocust(HttpLocust):
-#     host = os.getenv('TARGET_URL', "http://localhost:8000")
-#     min_wait = 1000
-#     max_wait = 10000
-
-# class StudentUser(BaseHttpLocust):
-#     task_set = StudentTaskSet
-
-# class TeacherUser(BaseHttpLocust):
-#     task_set = TeacherTaskSet
-
-# Copyright 2015-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file
-# except in compliance with the License. A copy of the License is located at
-#
-#     http://aws.amazon.com/apache2.0/
-#
-# or in the "license" file accompanying this file. This file is distributed on an "AS IS"
-# BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations under the License.
-
 import os
 import string
 import random
-from locust import HttpLocust, TaskSet, task
+from locust import HttpLocust
+from tasks import StudentTaskSet, TeacherTaskSet
 
-class MyTaskSet(TaskSet):
-    @task(1000)
-    def index(self):
-        response = self.client.get("/")
+class BaseHttpLocust(HttpLocust):
+    host = os.getenv('TARGET_URL', "http://localhost:8000")
+    min_wait = 1000
+    max_wait = 10000
 
-    # This task will 15 times for every 1000 runs of the above task
-    # @task(15)
-    # def about(self):
-    #     self.client.get("/blog")
+class StudentUser(BaseHttpLocust):
+    task_set = StudentTaskSet
 
-    # This task will run once for every 1000 runs of the above task
-    # @task(1)
-    # def about(self):
-    #     id = id_generator()
-    #     self.client.post("/signup", {"email": "example@example.com", "name": "Test"})
-
-class MyLocust(HttpLocust):
-    host = os.getenv('TARGET_URL', "http://localhost")
-    task_set = MyTaskSet
-    min_wait = 45
-    max_wait = 50
+class TeacherUser(BaseHttpLocust):
+    task_set = TeacherTaskSet
